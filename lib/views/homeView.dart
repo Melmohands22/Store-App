18d9ca8c94e7deec36a337bc61a2da05e4349c84
Melmohands22 <melmohands591@gmail.com>
2024-05-07 01:57:13 +0300

    add appBar and gridView.builder

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -6,6 +6,18 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Fancy Store',
+          style: TextStyle(fontSize: 32, color: Colors.black),
+        ),
+      ),
+      body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {}),
+    );
   }
 }
