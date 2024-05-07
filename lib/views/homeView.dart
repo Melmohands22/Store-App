5a64a3e9007d551595dcbbe6a6dcca3768da691a
Melmohands22 <melmohands591@gmail.com>
2024-05-07 11:44:50 +0300

    edit card

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -2,35 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
   static String id = 'HomeView';
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(FontAwesomeIcons.cartPlus, color: Colors.black),
-          )
-        ],
-        centerTitle: true,
-        elevation: 0,
-        title: Text(
-          'Fancy Store',
-          style: TextStyle(fontSize: 28, color: Colors.black),
-        ),
-      ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.cartPlus, color: Colors.black),
+            )
+          ],
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Fancy',
+                style: TextStyle(fontSize: 28, color: Colors.black),
+              ),
+              Text(
+                ' Store',
+                style: TextStyle(fontSize: 28, color: Colors.orange),
+              ),
+            ],
+          )),
       body: GridView.builder(
           gridDelegate:
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           itemBuilder: (context, index) {
-            return Card(
-              child: const Text(
-                'mohamed elmo',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
+            return Container(
+              height: 130,
+              width: 220,
+              child: Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 74,
+                        ),
+                        Text(
+                          'mohamed elmo',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              r'$220',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  FontAwesomeIcons.solidHeart,
+                                  color: Colors.red,
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
             );
           }),
     );
